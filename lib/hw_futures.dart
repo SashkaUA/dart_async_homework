d5400aab0dc2543cfcb6fb20e381ad6799aab43d
SashkaUA <siwuhov@gmail.com>
2025-07-14 17:41:52 +0300

    feat(DART-11): task #2 created

--- lib/hw_futures.dart
+++ lib/hw_futures.dart
@@ -8,7 +8,25 @@ Future<String> fetchName({bool isPrint = false}) async {
   return name;
 }
 
+Future<String> fetchAge({bool isPrint = false}) async {
+  final ageString = await Future.delayed(const Duration(microseconds: 1500), () => '25');
+  final age = int.parse(ageString);
+  final agePlural = switch (age) {
+    _ when age % 100 > 10 && age % 100 < 20 => 'років',
+    _ when age % 10 > 1 && age % 10 < 5 => 'роки',
+    _ when age % 10 == 1 => 'рік',
+    _ => 'років'
+  };
+
+  if (isPrint) print('Мені $age $agePlural');
+
+  return ageString;
+}
+
 void main() async {
   print('---------- Task 1 ----------');
   await fetchName(isPrint: true);
+
+  print('---------- Task 2 ----------');
+  await fetchAge(isPrint: true);
 }
